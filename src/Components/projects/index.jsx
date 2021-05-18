77089d2bea158833e7089c08e81e3a93e786b434
CronoxSoft <cronoxsoft@gmail.com>
2021-05-18 12:48:26 -0500

    dialog
    Added dialog, carousel to images

--- src/Components/projects/index.jsx
+++ src/Components/projects/index.jsx
@@ -1,10 +1,25 @@
+
 import React, { useEffect, useState } from "react";
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
 import './index.scss'
 import { db } from "../../firebaseConfig";
 
 const Projects = () => {
 
     const [projectsArray, setProjects] = useState([])
+    const [open, setOpen] = React.useState(false);
+    const [scroll, setScroll] = React.useState('paper');
+    const [project, setProject] = React.useState({
+        name: "",
+        icon: "",
+        description: "",
+        images: []
+    });
 
     const getProjects = () => {
         db.collection("projects").onSnapshot((querySnashot) => {
@@ -21,22 +36,75 @@ const Projects = () => {
         getProjects();
     }, []);
 
+    const handleClickOpen = (scrollType, project) => () => {
+        setProject(project)
+        setOpen(true);
+        setScroll(scrollType);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const descriptionElementRef = React.useRef(null);
+
+    useEffect(() => {
+        if (open) {
+            const { current: descriptionElement } = descriptionElementRef;
+            if (descriptionElement !== null) {
+                descriptionElement.focus();
+            }
+        }
+    }, [open]);
+
     return (
         <>
             <div className="container">
                 {
                     projectsArray.map((project, index) => {
                         return <div className="container__div">
-                                    <img src={project.icon} alt="" className="container__div-img"/>
-                                    <h1 className="container__div-h1" key={project.id}>{project.name}</h1>
-                                </div>
+                            <img
+                                src={project.icon}
+                                alt=""
+                                className="container__div-img"
+                                onClick={handleClickOpen('paper', project)}
+                            />
+                            <h1 className="container__div-h1" key={project.id}>{project.name}</h1>
 
+                        </div>
                     })
-                }
-            </div>
 
+                }
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    scroll={scroll}
+                    aria-labelledby="scroll-dialog-title"
+                    aria-describedby="scroll-dialog-description"
+                >
+                    <DialogTitle id="scroll-dialog-title">{project.name}</DialogTitle>
+                    <DialogContent dividers={scroll === 'paper'}>
+                        <DialogContentText
+                            id="scroll-dialog-description"
+                            ref={descriptionElementRef}
+                            tabIndex={-1}
+                        >
+                            <div className="dialog-container">
+                                <p>{project.description}</p>
+                                <div className="dialog-container__images">
+                                    <AliceCarousel autoPlay autoPlayInterval="3000" infinite="true">
+                                        {project.images.map(image => (
+                                            <img src={image} className="sliderimg" alt="" />
+                                        ))}
+                                    </AliceCarousel>
+                                </div>
+                            </div>
+                        </DialogContentText>
+                    </DialogContent>
+                </Dialog>
+            </div>
         </>
     )
 }
 
 export default Projects;
+
