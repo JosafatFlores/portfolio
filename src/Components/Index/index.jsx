8685d1b67cbc6a321f2c1c75a9251e7078eeb9c3
Josafat Flores <josafatfloresr@gmail.com>
2021-06-04 00:05:02 -0500

    index
    index added

--- src/Components/Index/index.jsx
+++ src/Components/Index/index.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { db } from "../../Services/Firebase/firebaseConfig";
+import ReactTextTransition, { presets } from "react-text-transition";
+import './index.scss'
 
 const Index = () => {
-    return(
-        <div className="container">
-            <img src="" alt="" />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quod tempora dicta fugit eos, expedita similique aspernatur ducimus, facilis velit maxime, veniam inventore alias! Facere consequatur modi et cum odit.</p>
+
+    const [photo, setPhoto] = useState("")
+    const [description, setDescription] = useState("")
+    const [skills, setSkils] = useState([])
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        getPersonalInfo()
+    }, [])
+
+    useEffect(() => {
+        const intervalId = setInterval(() =>
+            setIndex(index => index + 1),
+            5000
+        );
+        return () => clearTimeout(intervalId);
+    }, [])
+
+
+    const getPersonalInfo = () => {
+        db.collection("personalnfo").onSnapshot((querySnashot) => {
+
+            const skills = []
+
+            querySnashot.forEach((doc) => {
+                skills.push({ ...doc.data(), id: doc.id })
+            })
+
+            setPhoto(skills[0].photo)
+            setDescription(skills[0].description)
+            setSkils(skills[0].skills)
+        })
+    }
+
+
+    return (
+        <div className="main">
+            <img src={photo} alt="" />
+            <div className="main__div">
+
+            </div>
+            <div className="main__div">
+                <p>{description}</p>
+                {
+                    skills.length != 0
+                        ? <ReactTextTransition
+                            className="main__textTransition"
+                            text={skills[index % skills.length]}
+                            springConfig={presets.wobbly}
+                            style={{display:'inline-block'}}
+                        />
+                        : <p>Ingeniero en Sistemas Computacionales</p>
+                }
+
+            </div>
+
         </div>
     )
 }
